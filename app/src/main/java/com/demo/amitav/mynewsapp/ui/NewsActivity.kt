0fcaa04a0ebbe7b51fe4544e2fb48513f047bafb
amitav.singh <singh.amitav26@gmail.com>
2022-08-06 10:06:18 +0200

    1. Retrofit Setup

--- app/src/main/java/com/demo/amitav/mynewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/demo/amitav/mynewsapp/ui/NewsActivity.kt
@@ -1,19 +1,24 @@
 package com.demo.amitav.mynewsapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.demo.amitav.mynewsapp.R
 import kotlinx.android.synthetic.main.activity_news.*
+
 
 class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
 
-        bottomNavigationView.setupWithNavController(navController = newsNavHostFragment.findNavController())
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.newsNavHostFragment) as NavHostFragment?
+        val navController = navHostFragment!!.navController
+        bottomNavigationView.setupWithNavController(navController = navController)
 
     }
 }
---
+++ app/src/main/java/com/demo/amitav/mynewsapp/ui/models/Source.kt
@@ -0,0 +1,7 @@
+package com.demo.amitav.mynewsapp.ui.models
+
+
+data class Source(
+    val id: Any,
+    val name: String
+)
